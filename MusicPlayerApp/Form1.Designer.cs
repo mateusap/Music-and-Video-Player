d154b99fd22583732e6db2da8b9fe3f875a54c71
Mateus Silva <ma.aparecido.silva@hotmail.com>
2022-09-20 12:15:17 -0300

    Projeto utilizando windows forms para reproduzir arquivos

---
+++ MusicPlayerApp/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MusicPlayerApp
+{
+    partial class musicPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(musicPlayer));
+            this.listBoxMusic = new System.Windows.Forms.ListBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.lblFooter = new System.Windows.Forms.Label();
+            this.MediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxMusic
+            // 
+            this.listBoxMusic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxMusic.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxMusic.FormattingEnabled = true;
+            this.listBoxMusic.ItemHeight = 16;
+            this.listBoxMusic.Location = new System.Drawing.Point(520, 12);
+            this.listBoxMusic.Name = "listBoxMusic";
+            this.listBoxMusic.Size = new System.Drawing.Size(120, 164);
+            this.listBoxMusic.TabIndex = 1;
+            this.listBoxMusic.SelectedIndexChanged += new System.EventHandler(this.listBoxMusic_SelectedIndexChanged);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelect.Location = new System.Drawing.Point(520, 182);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(120, 40);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblFooter.AutoSize = true;
+            this.lblFooter.Location = new System.Drawing.Point(9, 225);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(157, 13);
+            this.lblFooter.TabIndex = 4;
+            this.lblFooter.Text = "Desenvolvido por: Mateus Silva";
+            // 
+            // MediaPlayer
+            // 
+            this.MediaPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MediaPlayer.Enabled = true;
+            this.MediaPlayer.Location = new System.Drawing.Point(12, 12);
+            this.MediaPlayer.Name = "MediaPlayer";
+            this.MediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("MediaPlayer.OcxState")));
+            this.MediaPlayer.Size = new System.Drawing.Size(484, 210);
+            this.MediaPlayer.TabIndex = 3;
+            // 
+            // musicPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(653, 244);
+            this.Controls.Add(this.lblFooter);
+            this.Controls.Add(this.MediaPlayer);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.listBoxMusic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "musicPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player";
+            this.Load += new System.EventHandler(this.musicPlayer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBoxMusic;
+        private System.Windows.Forms.Button btnSelect;
+        private AxWMPLib.AxWindowsMediaPlayer MediaPlayer;
+        private System.Windows.Forms.Label lblFooter;
+    }
+}
+
---
+++ MusicPlayerApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayerApp
+{
+    public partial class musicPlayer : Form
+    {
+        bool nextURL = false;
+        public musicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        //variaveis em array com título e caminho dos arquivos
+        String[] paths, files;
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            //selecionar música
+            OpenFileDialog fd = new OpenFileDialog();
+            //selecionar mais de um arquivo
+            fd.Multiselect = true;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                files = fd.SafeFileNames; //salva nome
+                paths = fd.FileNames; //salva caminho
+                //mostrar na listbox
+                for (int i =0; i<files.Length; i++)
+                {
+                    listBoxMusic.Items.Add(files[i]);
+                }
+            }
+        }
+
+        private void listBoxMusic_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MediaPlayer.URL = paths[listBoxMusic.SelectedIndex];
+        }
+
+        private void MediaPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 1)
+            {
+                nextURL = true;
+            }
+        }
+
+        private void musicPlayer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //função de fechar
+            this.Close();
+        }
+
+        
+    }
+}
